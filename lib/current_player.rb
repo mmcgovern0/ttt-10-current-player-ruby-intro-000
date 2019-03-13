5bd829ccef9a4db934df7baeedc66aaafe692485
Michael McGovern <mmcgovern.wework@gmail.com>
2019-03-13 19:17:16 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,5 @@
 def turn_count(board)
-  turn = 0 
-  board.each do |answer|
-    if answer == "X" || answer == "O"
-      turn += 1 
-    end
-end
-turn 
+  {
 
 def current_player(board)
   if (turn_count(board) % 2 == 0)
